1717cc1a7e81ad9413ef3f3dbe3c3a40e56d75fa
Nazmush Shakib <shakibgithub944>
2022-10-26 20:51:08 +0600

    first api for courses done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors');
 
 app.use(cors());
 
+const courses = require('./Data/course.json');
+
 app.get('/', (req, res) => {
     res.send('Course server is running.....');
+});
+
+app.get('/Course', (req, res) => {
+    res.send(courses);
 })
 
 
