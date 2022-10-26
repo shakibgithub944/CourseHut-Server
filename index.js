014277e39fd86be5d8bfc50ba2c5c143ef3f95b7
Nazmush Shakib <shakibgithub944>
2022-10-26 20:25:29 +0600

    server created and git added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Course server is running.....');
+})
+
+
+
+app.listen(port, () => {
+    console.log('server running on port ', port);
+})
+
